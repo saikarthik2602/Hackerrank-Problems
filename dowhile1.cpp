2593bcf47386d9753644c1052fc588817f274bfa
saikarthik2602 <56120462+saikarthik2602@users.noreply.github.com>
2019-10-09 18:32:57 +0530

    Add files via upload

---
+++ dowhile1.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int i=6;
+	do
+	{
+		cout<<i<<endl;     
+		i++;
+	}
+	while(i<5);
+}
+
+
+
+//the statment is wrong but the loop will run for 1 time
+//that fuctionality of do while
+//the o/p will be 6
---
+++ while1.cpp
@@ -0,0 +1,13 @@
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+int main(void)
+{
+	int i=0;
+	while(i<6)
+	{
+		cout<<i<<endl;
+		i++;
+	}
+
+}
